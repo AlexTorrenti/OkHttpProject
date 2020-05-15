0cb99b9e10ce3dbfc209a5e59dd55c616d8e3ea6
Alex Torrentí <alex@imaginagroup.com>
2020-05-15 18:10:45 +0200

    feature: certificate transparency method

--- app/src/main/java/com/imagina/okhttpproject/networking/SWAPIRetriever.kt
+++ app/src/main/java/com/imagina/okhttpproject/networking/SWAPIRetriever.kt
@@ -1,12 +1,12 @@
 package com.imagina.okhttpproject.networking
 
-import okhttp3.CertificatePinner
+import com.babylon.certificatetransparency.certificateTransparencyInterceptor
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class SWAPIRetriever {
-
+/*
     val hostname = "swapi.dev"
     val certificatePinner = CertificatePinner.Builder()
     .add(hostname, "sha256/eSiyNwaNIbIkI94wfLFmhq8/ATxm30i973pMZ569tZo=")
@@ -14,9 +14,20 @@ class SWAPIRetriever {
 
     // Con SSL Pinning
     private val client = OkHttpClient.Builder().certificatePinner(certificatePinner).build()
+*/
 
     // Sin SSL Pinning
-   // private val client = OkHttpClient.Builder().build()
+    // private val client = OkHttpClient.Builder().build()
+
+    val interceptor = certificateTransparencyInterceptor {
+        +"*.swapi.dev"
+    }
+
+    val client = OkHttpClient.Builder().apply {
+        addNetworkInterceptor(interceptor)
+    }.build()
+
+
     val service: APIService
 
     init {
